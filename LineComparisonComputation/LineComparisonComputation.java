2f609cd22696a202bbca7d2d1c80f4cf448a93fc
Dipti-Deshmane <itsmedipu17@gmail.com>
2023-11-06 23:49:30 +0530

    UC1 added

--- LineComparisonComputation/LineComparisonComputation.java
+++ LineComparisonComputation/LineComparisonComputation.java
@@ -6,17 +6,16 @@ public class LineComparisonComputation {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        // Read the coordinates of the first point
+
         System.out.print("Enter the coordinates of the first point (x1, y1): ");
         double x1 = scanner.nextDouble();
         double y1 = scanner.nextDouble();
 
-        // Read the coordinates of the second point
         System.out.print("Enter the coordinates of the second point (x2, y2): ");
         double x2 = scanner.nextDouble();
         double y2 = scanner.nextDouble();
 
-        // Calculate the length of the line
+
         double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 
         System.out.println("Length of the line = " + length);
