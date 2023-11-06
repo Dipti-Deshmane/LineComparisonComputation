13f9ee2893f7aff811c11e19714ebde3d35e6e9c
Dipti-Deshmane <itsmedipu17@gmail.com>
2023-11-06 18:17:40 +0530

    Master file added

---
+++ LineComparisonComputation/LineComparisonComputation.java
@@ -0,0 +1,7 @@
+package LineComparisonComputation;
+
+public class LineComparisonComputation {
+    public static void main(String[] args){
+        System.out.println("Welcome to the line comparison computation program");
+    }
+}
